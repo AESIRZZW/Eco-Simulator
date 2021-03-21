2104538273ab1ebc4bea4b1b6c4a726c67c1eb98
Alex Zhu <zhiwzhu@cisco.com>
2021-03-21 21:49:11 +0800

    Initialize the project

---
+++ src/test/java/com/worldify/ecosimulator/EcoSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldify.ecosimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
